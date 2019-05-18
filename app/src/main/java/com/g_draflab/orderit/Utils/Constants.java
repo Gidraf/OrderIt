2f6585b74d56ebe3d7f589c47139132b5ceed783
Gidraf Orenja <gidraf.orenja@andela.com>
2019-05-18 23:21:13 +0300

    feature(authentication):fetch auth API  - fetch signup api  - fetchsignin api  - fetch facebook api

---
+++ app/src/main/java/com/g_draflab/orderit/Utils/Constants.java
@@ -0,0 +1,11 @@
+package com.g_draflab.orderit.Utils;
+
+import android.content.Context;
+
+import com.g_draflab.orderit.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class Constants {
+
+}
